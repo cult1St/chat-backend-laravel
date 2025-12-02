3957c6038d3ab1590c478847801482f06532022c
wea1t <cultistsffmax@gmail.com>
2025-12-02 10:19:38 +0100

    Added Contacts Section

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\ApiResponder;
+use App\Http\Resources\ChatsResource;
+use App\Models\Conversation;
+use App\Models\ConversationParticipant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
+    use ApiResponder;
     public function index(){
-        
+        $user = Auth::user();
+
+        $chats = ConversationParticipant::where('user_id', $user->id)
+            ->whereHas('conversation', function($query){
+                $query->where('type', 'chat');
+            })
+            ->paginate(request()->get('take', 20));
+
+        $data = ChatsResource::collection($chats);
+
+        return $this->successResponse($data);
     }
 }
---
+++ app/Http/Resources/ChatsResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Contact;
+use App\Models\ConversationParticipant;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChatsResource extends JsonResource
+{
+
+    public function __construct($resource)
+    {
+        return parent::__construct($resource);
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $conversationId = $this->resource?->conversation_id;
+        $userId = $this->resource?->user_id;
+
+        ///get other participants and return them instead as chat objects
+        $otherPaticipant = ConversationParticipant::where('conversation_id', $conversationId)
+            ->whereNot('user_id', $userId)
+            ->with('user')
+            ->first();
+        $contact = Contact::where('inviter_id', $otherPaticipant->user?->id)->where('invitee_id', $userId)->first();
+        if(!$otherPaticipant) return [];
+        return [
+            'id' => $conversationId,
+            'display_name' => $contact ? $contact->name : $otherPaticipant->user?->phone,
+            'full_name' => $contact ? $contact->name : $otherPaticipant->user?->first_name ." ". $otherPaticipant->user?->last_name,
+            'image' => $otherPaticipant->user?->profile_photo_path,
+            'last_message' => $this->resource?->conversation->last_message
+        ];
+
+    }
+}
